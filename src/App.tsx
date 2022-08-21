26fa9e2060d5d1ec880b97d180834fa503c74e31
Rachel <borapark13@gmail.com>
2022-08-21 16:29:36 +0900

    add: main 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import SignIn from './pages/auth/SignIn';
 import SignUp from './pages/auth/SignUp';
+import Main from './pages/Main';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
